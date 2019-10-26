d7fd97e14c2a1ef6833aa9cb42282e38eba45707
Leah Kurtz <lfkurtz7@gmail.com>
2019-10-25 20:24:25 -0400

    made timer count backwards, used .hide to hide questions prior to starting quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,61 +26,33 @@ var correctArray = [
 ];
 
 window.onload = function () {
-    $("#startButton").on("click", start);
+$("#maincontent").hide();
+ $("#startButton").click(function() {
+    $("#maincontent").show();
+ }
+ 
 
-};
+ )};
 
-var intervalId;
-var stopTime = (time = "00:00")
-var clockRunning = false;
-var time = 0;
+$("#startButton").click(function () {
+    var time = 30;
+    setInterval(function () {
+      time--;
+      if (time >= 0) {
+        span = document.getElementById("time");
+        span.innerHTML = time;
+      }
+      if (time === 0) {
+        alert("Time's up!");
+        clearInterval(time);  
+      }
+    }, 1000);
+
+  });
+
+  //timer solution referenced from https://stackoverflow.com/questions/21670438/make-countdown-start-after-button-is-clicked
 
 
-function reset() {
-
-    time = 0;
-    $("#time").text("00:59");
-
-}
-function start() {
-    if (!clockRunning) {
-        intervalId = setInterval(count, 1000);
-        clockRunning = true;
-    }
-}
-function stop() {
-    
-    alert("Time's up!")
-    clearInterval(intervalId);
-    clockRunning = false;
-}
-
-function count() {
-
-    
-    time --;
-    var converted = timeConverter(time);
-    console.log(converted);
-    $("#time").text(converted);
-}
-function timeConverter(t) {
-
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
-    if (minutes === 0) {
-        minutes = "00";
-    }
-    else if (minutes < 10) {
-        minutes = "0" - minutes;
-    }
-
-    return minutes + ":" + seconds;
-}
 
 
 $(document).ready(function () {
@@ -91,45 +63,14 @@ $(document).ready(function () {
         $("#quiz").append("<input type='radio' name='q" + parseInt(i) + "' value='" + currentAnswers[1] + "'/>" + currentAnswers[1] + "<br/>");
         $("#quiz").append("<input type='radio' name='q" + parseInt(i) + "' value='" + currentAnswers[2] + "'/>" + currentAnswers[2] + "<br/>");
 
-        //alert("Press OK to begin!");
+        
 
-        // var time = $(".time").text();
-        // var interval = setInterval(function() {
-        //     var count = time.split(':');
-        //     var hour = count[0];
-        //     var minute = count[1];
-        //     var second = count[2];
-        //     var counter = $(".timer").find("span");
-        //     counter.eq(0).text(hour);
-        //     counter.eq(1).text(minute);
-        //     counter.eq(2).text(second);
-        //     if (hour === 0 && minute === 0) {
-        //         alert("60 seconds remaining")
-        //     }
-
-        //     if (second === 0) {
-        //         if (minute != 0) {
-        //             counter.eq(1).text(minute--);
-        //             second = 59;
-        //         }
-        //         else if (minute === 0 && second === 0)
-        //             alert("times up!")
-        //     }
-        // })
-
-
-
-
-
-
-        // input type='radio' name='q0' value='a1'>
+        
 
     }
 
-    // Now, append the button that calculates the score
-    $("#quiz").append('<button class="btn btn-info btn-lg" onclick="calculate_score();">Get Score</button>');
-
-
+    
+    $("#quiz").append('<button class="btn btn-info btn-lg" onclick="calculate_score();">Get Score</button>')
 
 
 
@@ -145,11 +86,9 @@ function calculate_score() {
             numCorrect++;
         }
 
-
-        // This returns the user answer for the first question (question with name q0)
-        // $("input[name='q0']:checked").val()
-
     }
     console.log("score: " + numCorrect + " out of 8 ")
     alert("score: " + numCorrect + " out of 8 ");
 }
+
+
